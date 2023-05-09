8a097e11b50e458cf6a4ee36d99d89bad508f1c5
Tanya Shylock <tourtanya@gmail.com>
2023-05-09 13:33:09 -0600

    step 2

---
+++ step2.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const fsP = require("fs/promises");
+const axios = require("axios");
+
+/** read contents of file specified in path, return contents
+ * or error if file path is incorrect/does not exist
+ */
+
+async function cat(path) {
+  let contents;
+
+  try {
+    contents = await fsP.readFile(path, "utf8");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log("file contents", contents);
+}
+
+const path = process.argv[2];
+
+// conditions for printing resp data to console:
+  // if file path, invoke cat();
+  // else, invoke webCat();
+if (path.includes("http")) {
+  webCat(path);
+} else {
+  cat(path);
+}
+
+
+/** make axios request, print to contents of response to console
+ */
+async function webCat(url) {
+  let resp;
+  try {
+    resp = await axios.get(url);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log("response data", resp.data);
+}
